6b41b8df4c51b10c044190ca09cb2ff2575c2f89
Vladimir Barikov <140310922+vladimirbarikov@users.noreply.github.com>
2025-11-22 16:14:35 +0300

    Update upload_api.py
    check the flask secret key is added

--- endpoints/upload_api.py
+++ endpoints/upload_api.py
@@ -2,36 +2,55 @@ import os
 import tempfile
 from flask import Flask, request, jsonify
 import requests
+from pathlib import Path
+from dotenv import load_dotenv
 
+# Load environment variables
+current_directory = Path(__file__).resolve().parent.parent.parent
+env_path = current_directory / '.env'
+load_dotenv(dotenv_path=env_path)
+
+# Creatу a Flask application
 app = Flask(__name__)
+
+# Setting the secret key from the environment variable
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
 @app.route('/upload-excel', methods=['POST'])
 def upload_excel():
-    # Проверяем, прислан ли файл
+    '''
+    Func accepts only specific file named "sample_mft_data.xlsx"
+    '''
+    # Check whether the file is uploaded
     if 'file' not in request.files:
         return jsonify({'error': 'Файл не передан'}), 400
 
     file = request.files['file']
 
-    # Проверяем расширение файла
+    # Validate the file name
+    expected_filename = 'sample_mft_data.xlsx'
+    if file.filename != expected_filename:
+        return jsonify({'error': f'Необходимо предоставить файл с именем "{expected_filename}".'}), 400
+
+    # Check the file extension
     if not file.filename.lower().endswith('.xlsx'):
         return jsonify({'error': 'Только файлы формата *.xlsx разрешены'}), 400
 
-    # Создаем временный файл для обработки
+    # Create a temporary file for processing
     temp_dir = tempfile.mkdtemp()
     temp_file_path = os.path.join(temp_dir, file.filename)
     file.save(temp_file_path)
 
     try:
-        # Отправляем запрос в Airflow
+        # Send a request to Airflow
         response = requests.post(
             'http://localhost:8080/api/v1/dags/excel_processing_dag/dagRuns',
             json={
                 'conf': {'file_path': temp_file_path},
                 'execution_date': 'NOW'
             },
-            auth=('airflow', 'airflow'),  # Если включена аутентификация
-            timeout=10  # Таймаут ожидания ответа в секундах
+            auth=('airflow', 'airflow'),
+            timeout=10  # Response timeout in seconds
         )
 
         if response.status_code != 200:
@@ -39,7 +58,7 @@ def upload_excel():
 
         return jsonify({'message': 'Файл успешно поставлен в очередь на обработку'}), 200
     finally:
-        # Пока не удаляем файл, так как он нужен для обработки
+        # Temporary file will be deleted in the DAG task
         pass
 
 if __name__ == '__main__':
